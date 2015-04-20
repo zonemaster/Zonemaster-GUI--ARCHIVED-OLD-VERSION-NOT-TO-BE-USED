81c78eec4e61ff15c064b0f70d654b50dd98fe6c
Calle Dybedahl <calle@init.se>
2015-04-20 12:13:50 +0200

    Remove unnecessary complexity.

--- bin/app.pl
+++ bin/app.pl
@@ -1,18 +1,7 @@
 #!/usr/bin/env perl
 use Dancer;
-use Plack::Builder;
-use Plack::App::File;
 
 use Zonemaster::GUI::Dancer::Frontend;
 use Zonemaster::GUI::Dancer::NoJsFrontend;
 
-
-my $app = sub {
-    my $env = shift;
-    my $request = Dancer::Request->new( env => $env );
-    Dancer->dance($request);
-};
-
-builder {
-    mount "/" => builder {$app};
-};
+dance;
