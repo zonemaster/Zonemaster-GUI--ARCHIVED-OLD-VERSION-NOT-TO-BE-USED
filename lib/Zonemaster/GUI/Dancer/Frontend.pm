6ade0bd8c2876ece86a75c9fb2dffe8cb2b14691
Calle Dybedahl <calle@init.se>
2015-04-20 14:27:59 +0200

    Do not JSON-convert Perl objects, should they ever appear. Do not mess with encodings (Dancer does that for us).

--- lib/Zonemaster/GUI/Dancer/Frontend.pm
+++ lib/Zonemaster/GUI/Dancer/Frontend.pm
@@ -3,13 +3,14 @@ package Zonemaster::GUI::Dancer::Frontend;
 use 5.14.2;
 use warnings;
 
+use Encode qw[decode_utf8];
+
 our $VERSION = '1.0.1';
 
 ###
 ### Fetch the FAQ source documents
 ###
 
-use Encode qw[decode_utf8 encode_utf8];
 use Text::Markdown 'markdown';
 use HTTP::Tiny;
 
@@ -54,13 +55,13 @@ get '/test/:id' => sub {
     $lang =~ s/,.*$//;
     my $result = $client->get_test_results( { params, language => $lang } );
     template 'index',
-      { result => to_json( $result, { allow_blessed => 1, convert_blessed => 1 } ), test_id => param( 'id' ) };
+      { result => to_json( $result ), test_id => param( 'id' ) };
 };
 
 get '/parent' => sub {
     my $result = $client->get_data_from_parent_zone( param( 'domain' ) );
     content_type 'application/json';
-    return to_json( { result => $result }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { result => $result } );
 };
 
 get '/version' => sub {
@@ -68,56 +69,54 @@ get '/version' => sub {
     content_type 'application/json';
     my $ip = request->address;
     $ip =~ s/::ffff:// if ( $ip =~ /::ffff:/ );
-    return to_json( { result => $result . ", IP address: $ip" }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { result => $result . ", IP address: $ip" } );
 };
 
 get '/check_syntax' => sub {
-    my $data = from_json( encode_utf8( param( 'data' ) ) );
+    my $data = from_json( param( 'data' ) );
     my $result = $client->validate_syntax( {%$data} );
     content_type 'application/json';
-    return to_json( { result => $result }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { result => $result } );
 };
 
 get '/history' => sub {
-    my $data = from_json( encode_utf8( param( 'data' ) ) );
+    my $data = from_json( param( 'data' ) );
     my $result = $client->get_test_history( { frontend_params => {%$data}, limit => 200, offset => 0 } );
     content_type 'application/json';
-    return to_json( { result => $result }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { result => $result } );
 };
 
 get '/resolve' => sub {
     my $data   = param( 'data' );
     my $result = $client->get_ns_ips( $data );
     content_type 'application/json';
-    return to_json( { result => $result }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { result => $result } );
 };
 
 post '/run' => sub {
-    my $data = from_json( encode_utf8( param( 'data' ) ) );
+    my $data = from_json( param( 'data' ) );
     $data->{client_id}      = 'Zonemaster Dancer Frontend';
-    $data->{client_version} = $VERSION;
+    $data->{client_version} = __PACKAGE__->VERSION;
     my $job_id = $client->start_domain_test( {%$data} );
     content_type 'application/json';
-    return to_json( { job_id => $job_id }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { job_id => $job_id } );
 };
 
 get '/progress' => sub {
     my $progress = $client->test_progress( param( 'id' ) );
     header( 'Cache-Control' => 'no-store, no-cache, must-revalidate' );
     content_type 'application/json';
-    return to_json( { progress => $progress }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { progress => $progress } );
 };
 
 get '/result' => sub {
     my $result = $client->get_test_results( {params} );
     content_type 'application/json';
-    return to_json( { result => $result }, { allow_blessed => 1, convert_blessed => 1 } );
+    return to_json( { result => $result } );
 };
 
 get '/faq' => sub {
-    my %allparams = params;
-
-    return to_json( { FAQ_CONTENT => $faqs{ $allparams{lang} } } );
+    return to_json( { FAQ_CONTENT => $faqs{ param('lang') } } );
 };
 
 true;
