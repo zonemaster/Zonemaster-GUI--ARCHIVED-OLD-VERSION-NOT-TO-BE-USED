d00e439b68d6a1aa9f10a13332214da97f769cae
Michal TOMA <toma@nic.fr>
2015-06-09 19:09:31 +0200

    Fixes #115

--- lib/Zonemaster/GUI/Dancer/Client.pm
+++ lib/Zonemaster/GUI/Dancer/Client.pm
@@ -1,5 +1,5 @@
 package Zonemaster::GUI::Dancer::Client;
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.2_01';
 
 use strict;
 use warnings;
--- lib/Zonemaster/GUI/Dancer/Frontend.pm
+++ lib/Zonemaster/GUI/Dancer/Frontend.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Encode qw[decode_utf8];
 
-our $VERSION = '1.0.2';
+our $VERSION = '1.0.2_01';
 
 ###
 ### Fetch the FAQ source documents
