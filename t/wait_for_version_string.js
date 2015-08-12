20c57d9f6db4791b30c585a695f2b87f38fc3e2d
Michal TOMA <toma@afnic.fr>
2015-08-12 19:00:47 +0200

    Travis: phantomjs 2.0 tests - 11

--- t/wait_for_version_string.js
+++ t/wait_for_version_string.js
@@ -1,5 +1,5 @@
 
-function waitFor(testFx, onReady, timeOutMillis) {
+function waitFor(testFx, onReady, onTimeout, timeOutMillis) {
     var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 60000, //< Default Max Timout is 30s
         start = new Date().getTime(),
         condition = false,
@@ -11,7 +11,7 @@ function waitFor(testFx, onReady, timeOutMillis) {
                 if(!condition) {
                     // If condition still not fulfilled (timeout but condition is 'false')
                     console.log("'waitFor()' timeout");
-                    phantom.exit(1);
+                    onTimeout();
                 } else {
                     // Condition fulfilled (timeout and/or condition is 'true')
 //                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
@@ -19,7 +19,7 @@ function waitFor(testFx, onReady, timeOutMillis) {
                     clearInterval(interval); //< Stop this interval
                 }
             }
-        }, 5000); //< repeat check every 250ms
+        }, 250); //< repeat check every 250ms
 };
 
 
@@ -38,6 +38,10 @@ else {
 			console.log('  ', item.file, ':', item.line);
 		});
 	};
+	
+	page.onConsoleMessage = function(msg) {
+		system.stdout.writeLine('page.evaluate console: ' + msg);
+	};
 
 	page.open(url, function (status) {
 		// Check for page load success
@@ -47,8 +51,33 @@ else {
 			console.log("page ["+url+"] loaded");
 			console.log('Stripped down page text:\n' + page.plainText);
 			
+			page.evaluate(function() {
+				console.log('page.evaluate:start');
+				var getElementByXpath = function (path) {
+					return document.evaluate(path, document, null, 9, null).singleNodeValue;
+				};
+								
+				var clickNode = function click(el){
+					var ev = document.createEvent("MouseEvent");
+					ev.initMouseEvent(
+						"click",
+						true /* bubble */, true /* cancelable */,
+						window, null,
+						0, 0, 0, 0, /* coordinates */
+						false, false, false, false, /* modifier keys */
+						0 /*left*/, null
+					);
+					el.dispatchEvent(ev);
+				};
+
+				var element = getElementByXpath("//a[contains(., 'FAQ')]");
+				console.log("getElementByXpath:done");
+				clickNode(element);
+				console.log('Clicked on FAQ');
+			});
+
 			waitFor(function() {
-console.log('Stripped down page text:\n' + page.plainText);
+console.log('\n------------------------------------\nStripped down page text:\n' + page.plainText);
 				return page.evaluate(function() {
 					var getElementByXpath = function (path) {
 						return document.evaluate(path, document, null, 9, null).singleNodeValue;
@@ -57,7 +86,7 @@ console.log('Stripped down page text:\n' + page.plainText);
 					return (getElementByXpath("//div[@version and contains(., 'TRAVIS')]") ? true : false);
 //					return (getElementByXpath("//body") ? true : false);
 				});
-			}, function() {
+			}, function() { //onReady
 
 				var result = page.evaluate(function() {
 					return "RESULT:OK";
@@ -66,7 +95,10 @@ console.log('Stripped down page text:\n' + page.plainText);
 				console.log(result);
 				
 				phantom.exit(0);
+			}, function() { //onTimeout
+				console.log("TIMEOUT");
 			});
+			
 		}
 	});
 }
